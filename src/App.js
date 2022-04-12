2bb6c9789dcc8eb587d972566fc5a8e7ca3b05c5
Carlie Smith <carliesmith011@gmail.com>
2022-04-12 10:15:09 -0400

    trying to resolve index files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './src/utils/App.css';
+import About from './components/About';
+
+function App() {
+  return (
+    <div >
+        <main>
+            <About></About>
+        </main>
+      
+    </div>
+  );
+}
+
+export default App;
